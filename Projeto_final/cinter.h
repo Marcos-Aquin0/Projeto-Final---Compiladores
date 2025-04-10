326f1c584ce8e61a3e3e2202537119c3373f3811
Marcos Aquino <marcos.aquino@unifesp.br>
2025-04-10 15:15:51 -0300

    fix op alloc

--- Projeto_final/cinter.h
+++ Projeto_final/cinter.h
@@ -26,7 +26,7 @@ typedef enum {
     OP_RETURN,      // Retorno de função
     OP_ARRAY_LOAD,  // Carrega valor de um array
     OP_ARRAY_STORE, // Armazena valor em um array
-    // OP_ALLOC,       // Aloca memória para variáveis locais
+    OP_ALLOC,       // Aloca memória para variáveis locais
     OP_FUNCTION,    // Definição de função
     OP_END          // Fim de função
 } OperationType;
